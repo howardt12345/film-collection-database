82425999de02a39ad85304d966220de3e64217d6
howardt12345 <howardt12345@gmail.com>
2024-09-25 14:44:57 -0400

    ts

--- env.sh
+++ env.sh
@@ -8,5 +8,5 @@ do
     # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
 
     # sed JS and CSS only
-    find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+    find /usr/share/nginx/html -type f \( -name '*.ts' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
 done
