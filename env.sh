d4113b32b9fbe280c62c7a749fef6e50de6f330c
Howard Tseng <howardt12345@gmail.com>
2024-09-25 15:01:59 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-for i in $(env | grep MY_APP_)
+
+# Define the directory where your Vue app is built
+VUE_APP_DIR="/usr/share/nginx/html"
+
+for i in $(env | grep VUE_APP_)
 do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2-)
-    echo $key=$value
-    # sed All files
-    # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
-
-    # sed JS and CSS only
-    find /usr/share/nginx/html -type f \( -name '*.ts' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+    echo "Replacing $key with $value"
+    
+    # Escape special characters in the value
+    escaped_value=$(printf '%s\n' "$value" | sed -e 's/[\/&]/\\&/g')
+    
+    # Replace in JS files (including .js, .ts, and .vue files)
+    find $VUE_APP_DIR -type f \( -name '*.js' -o -name '*.ts' -o -name '*.vue' \) -exec sed -i "s|$key|$escaped_value|g" {} +
+    
+    # Replace in CSS files
+    find $VUE_APP_DIR -type f -name '*.css' -exec sed -i "s|$key|$escaped_value|g" {} +
+    
+    # Replace in HTML files
+    find $VUE_APP_DIR -type f -name '*.html' -exec sed -i "s|$key|$escaped_value|g" {} +
 done
+
+# Run the Nginx server
+nginx -g 'daemon off;'
